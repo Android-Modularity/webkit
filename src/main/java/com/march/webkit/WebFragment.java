648d77cd4a9483a496567dd8b16b74ea8725031c
luyuan <1101873740@qq.com>
2018-04-24 11:45:11 +0800

    [add] WebFragment

---
+++ src/main/java/com/march/webkit/WebFragment.java
@@ -0,0 +1,55 @@
+package com.march.webkit;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.text.TextUtils;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+
+/**
+ * CreateAt : 2018/4/24
+ * Describe :
+ *
+ * @author chendong
+ */
+public class WebFragment extends Fragment {
+
+    private IWebView mIWebView;
+    private String mUrl;
+
+    public static WebFragment newInst(Bundle args) {
+        WebFragment webFragment = new WebFragment();
+        webFragment.setArguments(args);
+        return webFragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        mIWebView = WebKit.createWebView(getActivity());
+        if (getArguments() != null) {
+            mUrl = getArguments().getString(WebKit.KEY_URL);
+        }
+        return (View) mIWebView;
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        if (!TextUtils.isEmpty(mUrl)) {
+            mIWebView.loadPage(mUrl);
+        }
+    }
+
+    public boolean onBackPressed() {
+        return mIWebView.onBackPressed();
+    }
+}
--- src/main/java/com/march/webkit/WebKit.java
+++ src/main/java/com/march/webkit/WebKit.java
@@ -1,7 +1,10 @@
 package com.march.webkit;
 
+import android.app.Activity;
 import android.app.Application;
 
+import com.march.webkit.sys.SysWebView;
+import com.march.webkit.x5.X5WebView;
 import com.tencent.smtt.sdk.QbSdk;
 
 /**
@@ -12,16 +15,41 @@ import com.tencent.smtt.sdk.QbSdk;
  */
 public class WebKit {
 
+    static class WebKitConfig {
+        int webkitType = CORE_SYS;
+    }
+
+    public static final String KEY_URL = "key-url";
+    public static final int CORE_SYS = 0;
+    public static final int CORE_X5 = 1;
+
+    private static WebKitConfig sWebKitConfig;
     private static WebKitInjector sWebKitInjector = WebKitInjector.EMPTY;
 
     public static WebKitInjector getInjector() {
         return sWebKitInjector;
     }
 
-    public static void init(Application application, WebKitInjector injector) {
-        if(injector!=null) {
+    public static WebKitConfig getWebKitConfig() {
+        return sWebKitConfig;
+    }
+
+    public static IWebView createWebView(Activity activity) {
+        IWebView iWebView;
+        if (WebKit.getWebKitConfig().webkitType == WebKit.CORE_SYS) {
+            iWebView = new SysWebView(activity);
+        } else {
+            iWebView = new X5WebView(activity);
+        }
+        return iWebView;
+    }
+
+    public static void init(Application application, int type, WebKitInjector injector) {
+        if (injector != null) {
             sWebKitInjector = injector;
         }
+        sWebKitConfig = new WebKitConfig();
+        sWebKitConfig.webkitType = type;
         QbSdk.initX5Environment(application, new QbSdk.PreInitCallback() {
             @Override
             public void onCoreInitFinished() {
